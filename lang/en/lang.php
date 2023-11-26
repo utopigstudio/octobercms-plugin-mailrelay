4650ed3447b8c50f4e374ba74940154207b235b2
Maria Vilaró <maria@utopigstudio.com>
2023-11-26 12:55:31 +0100

    cleanup, doc, prepare to publish

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -7,10 +7,13 @@ return [
     ],
 
     'permissions' => [
-        'configure' => 'Configure MailRelay',
+        'configure' => 'Configure MailRelay API access',
     ],
 
     'settings' => [
-        'description' => 'Configure MailRelay API access.'
+        'description' => 'Configure MailRelay API access.',
+        'account_name' => 'MailRelay account name',
+        'api_key' => 'MailRelay API key',
+        'api_key_comment' => 'Get an API Key from https://your-account-name.ipzmarketing.com/admin/api_keys/',
     ]
 ];
